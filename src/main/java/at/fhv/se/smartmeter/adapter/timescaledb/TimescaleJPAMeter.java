38bd32605ce637720b2526376883891491929399
xBlade58 <mert_oeztuerk58@outlook.de>
2023-12-10 13:13:10 +0100

    Use UUID in JPA interfaces

--- src/main/java/at/fhv/se/smartmeter/adapter/timescaledb/TimescaleJPAMeter.java
+++ src/main/java/at/fhv/se/smartmeter/adapter/timescaledb/TimescaleJPAMeter.java
@@ -1,9 +1,11 @@
 package at.fhv.se.smartmeter.adapter.timescaledb;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import at.fhv.se.smartmeter.adapter.timescaledb.mapper.MeterDBEntity;
 
-public interface TimescaleJPAMeter extends JpaRepository<MeterDBEntity, Integer> {
+public interface TimescaleJPAMeter extends JpaRepository<MeterDBEntity, UUID> {
     
 }
